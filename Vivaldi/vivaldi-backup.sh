e838bebb14f7ff0c848dec39cdeee451c6fccbcc
FabCre <fabien@await.fr>
2022-03-13 10:49:30 +0100

    feat(script): add vivaldi script

--- Vivaldi/vivaldi-backup.sh
+++ Vivaldi/vivaldi-backup.sh
@@ -10,12 +10,11 @@ fi
 
 TODAY=$(date '+%Y-%m-%d')
 BACKUP_FOLDER_PATH=
-BACKUP_FILENAME=thunderbird-profile-$TODAY
-THUNDERBIRD_PROFILE_PATH=
+BACKUP_FILENAME=vivaldi-profile-$TODAY
+VIVALDI_PROFILE_PATH=
 LOCK_FILE_PATH=
-IS_THUNDERBIRD_RUNNING=0
-ATTEMPTS_TO_STOP_THUNDERBIRD=5
-BACKUPS_OLDER_THAN=4
+IS_VIVALDI_RUNNING=0
+ATTEMPTS_TO_STOP_VIVALDI=5
 
 # Check if the file already exists
 test -e "$BACKUP_FOLDER_PATH/$BACKUP_FILENAME.tar.gz" &&
@@ -32,46 +31,46 @@ cat /dev/null >>$LOCK_FILE_PATH
 read -r lastPID <$LOCK_FILE_PATH
 
 # if lastPID is not null and a process with that pid exists , exit
-[ -n "$lastPID" ] && [ -d /proc/"$lastPID" ] && echo "A Thunderbird backup is already running." && exit
+[ -n "$lastPID" ] && [ -d /proc/"$lastPID" ] && echo "A Vivaldi backup is already running." && exit
 
 # not running, save my pid in the lock file
 echo $$ >$LOCK_FILE_PATH
 
-echo "Thunderbird backup starting in mode $MODE, backup folder: $BACKUP_FOLDER_PATH"
+echo "Vivaldi backup starting in mode $MODE, backup folder: $BACKUP_FOLDER_PATH"
 
 # -----------------------------------------------------------------------
 
-# If thunderbird running ask user to close it
+# If vivaldi running ask user to close it
 # If user answer No, exiting
 # If user say Yes, then check again, if it still running ask again (5 attempts)
 
-for ((i = 1; i <= ATTEMPTS_TO_STOP_THUNDERBIRD; i++)); do
-  if pgrep "thunderbird" >/dev/null; then
+for ((i = 1; i <= ATTEMPTS_TO_STOP_VIVALDI; i++)); do
+  if pgrep "firefox" >/dev/null; then
 
-    IS_THUNDERBIRD_RUNNING=1
+    IS_VIVALDI_RUNNING=1
 
     if [ ${MODE} = "Auto" ]; then
-      echo "Killing Thunderbird process before backup."
-      killall thunderbird
+      echo "Killing Vivaldi process before backup."
+      killall vivaldi
       sleep 5
       break
     fi
 
     # On the first round of the loop, then after until five round
     if [ $i -eq 1 ]; then
-      MSG="Before starting backup, Thunderbird must be closed."
+      MSG="Before starting backup, Vivaldi must be closed."
     else
-      MSG="Thunderbird still running."
+      MSG="Vivaldi still running."
     fi
 
-    echo "$MSG Could you please close Thunderbird? ($i / $ATTEMPTS_TO_STOP_THUNDERBIRD attempts)"
+    echo "$MSG Could you please close Vivaldi? ($i / $ATTEMPTS_TO_STOP_VIVALDI attempts)"
 
     # Open Modal to advise user, check "man zenity"
     zenity \
       --width=400 \
       --question \
-      --title="Thunderbird backup" \
-      --text="$MSG\nCould you please close Thunderbird? \n($i / $ATTEMPTS_TO_STOP_THUNDERBIRD attempts)"
+      --title="Vivaldi backup" \
+      --text="$MSG\nCould you please close Vivaldi? \n($i / $ATTEMPTS_TO_STOP_VIVALDI attempts)"
 
     # if user press No button
     if [ $? -eq 1 ]; then
@@ -84,13 +83,13 @@ for ((i = 1; i <= ATTEMPTS_TO_STOP_THUNDERBIRD; i++)); do
   fi
 done
 
-# If user say five times that he close thunderbird but it still running, show error and exiting
+# If user say five times that he close vivaldi but it still running, show error and exiting
 
-if pgrep "thunderbird" >/dev/null; then
+if pgrep "vivaldi" >/dev/null; then
   if [ ${MODE} = "Manual" ]; then
-    zenity --error --text "Can't do backup, Thunderbird is still running. \nPlease kill the process before backup. \nThunderbird backup stop." --width=400
+    zenity --error --text "Can't do backup, Vivaldi is still running. \nPlease kill the process before backup. \nVivaldi backup stop." --width=400
   fi
-  echo "Error: Can't do backup, Thunderbird is still running. Please kill the process before backup. Thunderbird backup stop."
+  echo "Error: Can't do backup, Vivaldi is still running. Please kill the process before backup. Vivaldi backup stop."
   exit
 fi
 
@@ -98,10 +97,10 @@ fi
 
 # Now we can do backup, note: to disable silent mode remove below ">/dev/null"
 
-echo "Thunderbird backup in progress..."
-pushd $THUNDERBIRD_PROFILE_PATH >/dev/null || exit
+echo "Vivaldi backup in progress..."
+pushd $VIVALDI_PROFILE_PATH >/dev/null || exit
 cd ..
-tar -czf "$BACKUP_FOLDER_PATH/$BACKUP_FILENAME.tar.gz" -P $THUNDERBIRD_PROFILE_PATH
+tar -czf "$BACKUP_FOLDER_PATH/$BACKUP_FILENAME.tar.gz" -P $VIVALDI_PROFILE_PATH
 popd >/dev/null || exit
 
 # -----------------------------------------------------------------------
@@ -111,27 +110,27 @@ popd >/dev/null || exit
 test -e $LOCK_FILE_PATH &&
 echo "Removing the lock file..." &&
 rm -f $LOCK_FILE_PATH &&
-echo "The Lock file successfully removed."||
+echo "The Lock file successfully removed." ||
 echo "The Lock file has already been removed."
 
 # -----------------------------------------------------------------------
 
 # Check if there is file older than four days and remove it
 
-find $BACKUP_FOLDER_PATH/* -type f -mtime +$BACKUPS_OLDER_THAN -exec rm -f {} \; &&
-echo "Backup older than $BACKUPS_OLDER_THAN days are successfully removed."
+find $BACKUP_FOLDER_PATH/* -type f -mtime +4 -exec rm -f {} \; &&
+echo "Backup older than 4 days are successfully removed."
 
 # -----------------------------------------------------------------------
 
 # Show notify message that backup is created
 
 if [ -f "$BACKUP_FOLDER_PATH/$BACKUP_FILENAME.tar.gz" ]; then
-  echo "Thunderbird backup successfully created."
-  notify-send "Thunderbird backup successfully created."
+  echo "Vivaldi backup successfully created."
+  notify-send "Vivaldi backup successfully created."
 else
-  echo "Error: Thunderbird backup hasn't created. unexpected error occurred."
+  echo "Error: Vivaldi backup hasn't created. unexpected error occurred."
   if [ ${MODE} = "Manual" ]; then
-    zenity --error --text "Thunderbird backup hasn't created.\nUnexpected error occurred." --width=400
+    zenity --error --text "Vivaldi backup hasn't created.\nUnexpected error occurred." --width=400
   fi
 fi
 
